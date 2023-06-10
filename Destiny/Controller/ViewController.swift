0f98472b65b7984ac970a491035506dc67efa31b
mohammadgdz <mhd.gdz@gmail.com>
2023-06-10 17:49:25 +0330

    add buttons and labels

--- Destiny/Controller/ViewController.swift
+++ Destiny/Controller/ViewController.swift
@@ -9,11 +9,17 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var storyLable: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func choice1(_ sender: UIButton) {
+    }
+    @IBAction func choice2(_ sender: UIButton) {
+    }
+    
 }
 
