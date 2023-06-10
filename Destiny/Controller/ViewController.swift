fe97bea407a9bb2cbaa433521f9bcbe897ab293e
mohammadgdz <mhd.gdz@gmail.com>
2023-06-10 20:33:27 +0330

    add functions & stories

--- Destiny/Controller/ViewController.swift
+++ Destiny/Controller/ViewController.swift
@@ -13,12 +13,29 @@ class ViewController: UIViewController {
     @IBOutlet weak var choice1Button: UIButton!
     @IBOutlet weak var choice2Button: UIButton!
     
+    var story = StoryBrain()
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        updateUI()
+        
+    
     }
     
     @IBAction func choiceMade(_ sender: UIButton) {
+        
+        story.nextStory(userChoice: sender.currentTitle!)
+        
+        updateUI()
+        
+    }
+    
+    func updateUI() {
+        storyLable.text = story.getStoryTitle()
+        choice1Button.setTitle(story.getChoice1(), for: .normal)
+        choice2Button.setTitle(story.getChoice2(), for: .normal)
         
     }
     
--- Destiny/Model/StoryBrain.swift
+++ Destiny/Model/StoryBrain.swift
@@ -6,3 +6,52 @@
 //
 
 import Foundation
+
+struct StoryBrain {
+    
+    var storyNumber = 0
+    
+    let stories = [
+    Story(
+        title: "You see a fork in the road.",
+        choice1: "Take a left.",
+        choice2: "Take a right.",
+        choice1Destination: 1,
+        choice2Destination: 2),
+    Story(
+        title: "You see a tiger.",
+        choice1: "Shout for help.",
+        choice2: "Play dead.",
+        choice1Destination: 0,
+        choice2Destination: 0),
+    Story(
+        title: "You find a treasur chest.",
+        choice1: "Open it.",
+        choice2: "Check for traps.",
+        choice1Destination: 0,
+        choice2Destination: 0)
+    ]
+    
+    func getStoryTitle() -> String {
+        return stories[storyNumber].title
+    }
+    
+    func getChoice1() -> String {
+        return stories[storyNumber].choice1
+    }
+    
+    func getChoice2() -> String {
+        return stories[storyNumber].choice2
+    }
+    
+    mutating func nextStory(userChoice:String) {
+        let currentStory = stories[storyNumber]
+        if userChoice == currentStory.choice1 {
+            storyNumber = currentStory.choice1Destination
+        } else if userChoice == currentStory.choice2 {
+            storyNumber = currentStory.choice2Destination
+        }
+    }
+
+}
+
